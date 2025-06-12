58ce17e7131d23137b68f72bc49a6205f19c2225
anhhuy <huy11@dukkungu.com>
2025-06-12 11:57:37 +0700

    update dish img

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const config = require("./config/config");
+require('dotenv').config(); // Phải đặt ở trên cùng
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
